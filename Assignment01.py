3d238c0fe036b43c43560735366e3cf6d04c1e93
etp567 <46685283+etp567@users.noreply.github.com>
2019-01-27 17:02:06 -0600

    final

---
+++ Assignment01.py
@@ -0,0 +1,29 @@
+#Steps 1 and 2: Adding numeric objects
+x = 1
+y = 2
+
+z = x + y
+
+print z
+
+#Steps 3-6: Concatenating string objects
+phrase_1 = "It's"
+phrase_2 = " raining"
+phrase_3 = " cats"
+phrase_4 = " and"
+phrase_5 = " dogs"
+
+full_phrase = phrase_1 + phrase_2 + phrase_3 + phrase_4 + phrase_5
+
+print full_phrase
+
+print full_phrase + " contains " + str(len(full_phrase)) + " characters."
+
+#Steps 7 and 8: Path to directory
+my_path = r"C:\Users\Ethan Paik\Desktop\GISProgramming"
+
+print my_path + " is the working directory."
+
+
+
+
